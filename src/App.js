41b6238221afd19724b285379a741db4ce040a0d
Karolin Rafalski <kr2174@caa.columbia.edu>
2019-05-05 16:24:57 -0400

    revert to start state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css'
 import About from './components/About.js'
 import Contact from './components/Contact.js'
 import Location from './components/Location'
 import Menu from './components/Menu.js'
-
+import Navigation from './components/Navigation.js'
 
 class App extends React.Component {
   render () {
     return (
-      <Router>
-        <div className="container">
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/locations">Locations</Link>
-          </nav>
-          <Route path="/" exact component={Menu} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/locations" component={Location} />
-        </div>
-      </Router>
+      <div className="container">
+        <Navigation />
+        <About />
+        <Contact />
+        <Location />
+        <Menu />
+      </div>
     )
   }
 }
